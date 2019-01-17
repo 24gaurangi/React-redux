116c26a97bbc3e622652b60d91e6fce0829b0e27
24gaurangi <gwanjari19@gmail.com>
2019-01-16 23:01:48 -0800

    CSS Changes

--- src/Components/SignedOut.js
+++ src/Components/SignedOut.js
@@ -5,8 +5,8 @@ import {NavLink} from 'react-router-dom'
 const SignedOut = () => {
   return(
     <ul className="right">
-      <li><NavLink to="/signin" className="black-text"> Login </NavLink></li>
-      <li><NavLink to="/signup" className="black-text"> Sign Up </NavLink></li>
+      <li><NavLink to="/signin" className="white-text"> <h5>Login</h5> </NavLink></li>
+      <li><NavLink to="/signup" className="white-text"> <h5>Sign Up </h5></NavLink></li>
     </ul>
     )
   }
